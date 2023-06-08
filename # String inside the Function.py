5ebb3f71c4190fbd0fb5f50e52c54975f84cdd67
vallemalleswararao <134518813+vallemalleswararao@users.noreply.github.com>
2023-06-08 17:10:00 +0530

    Add files via upload

---
+++ # String inside the Function.py
@@ -0,0 +1,14 @@
+# String inside the Function
+#python program to reverse a string
+
+#define the function
+def given_function(x):
+
+#slice the string and retun
+ return x[::-1]
+
+#function call
+input_text = given_function("1234abcd ")
+
+#print the result
+print(input_text)
