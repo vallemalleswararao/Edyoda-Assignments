f079d6999f42fe97f67a0ab312016c2009577f79
vallemalleswararao <134518813+vallemalleswararao@users.noreply.github.com>
2023-06-08 17:08:31 +0530

    Add files via upload

---
+++ # Game of Functions.py
@@ -0,0 +1,14 @@
+# Game of Functions
+#phython program to print sum of all the numbers in the list
+def sum(numbers):
+    total = 0
+    for x in numbers:
+        total += x
+    return total
+print(sum((8, 2, 3, 0, 7)))
+
+
+
+
+
+    
