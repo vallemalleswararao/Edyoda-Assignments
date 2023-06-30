2c9f5f9ca44d94e00da90c28984594202bd91829
vallemalleswararao <134518813+vallemalleswararao@users.noreply.github.com>
2023-06-30 19:52:30 +0530

    Add files via upload

---
+++ employee data.py
@@ -0,0 +1,39 @@
+{
+  "employees": [
+    {
+      "Name": "John Doe",
+      "DOB": "1990-05-10",
+      "Height": 175,
+      "City": "New York",
+      "State": "NY"
+    },
+    {
+      "Name": "Jane Smith",
+      "DOB": "1985-12-15",
+      "Height": 160,
+      "City": "Los Angeles",
+      "State": "CA"
+    },
+    {
+      "Name": "Mark Johnson",
+      "DOB": "1992-07-20",
+      "Height": 180,
+      "City": "Chicago",
+      "State": "IL"
+    },
+    {
+      "Name": "Emily Davis",
+      "DOB": "1988-02-28",
+      "Height": 165,
+      "City": "Houston",
+      "State": "TX"
+    },
+    {
+      "Name": "Michael Wilson",
+      "DOB": "1995-09-05",
+      "Height": 170,
+      "City": "Miami",
+      "State": "FL"
+    }
+  ]
+}
